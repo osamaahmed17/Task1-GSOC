e913ea97cc64ad5cc5d05a9cc5352e3df0210fb5
osamaahmed17 <osamaahmedtahir17@gmail.com>
2022-03-19 18:11:28 +0500

    reconstructed Result Controller

--- controller/resultController.js
+++ controller/resultController.js
@@ -1,49 +1,118 @@
-var sokcetUtility = require("../utility/socketUtility");
-
-
-
+let playerScore = 0;
+let computerScore = 0;
+let moves = 0;
 class resultController {
+
     constructor() {
-        this.getResult = this.getResult.bind(this);
-        this.getWinner = this.getWinner.bind(this);
-    }
+        this.winGame = this.winGame.bind(this);
+        this.playGame = this.playGame.bind(this);
+        this.overGame = this.overGame.bind(this);
 
-    async getResult(roomID) {
-        var choice1 = "";
-        var choice2 = "";
-        var players = [];
-        var getWinner = await this.getWinner(choice1, choice2);
-        var getRoom = await this.getRoomID(roomID, players, getWinner);
 
     }
 
-    async getRoomID(roomID, players, getWinner) {
-        sokcetUtility = socketUtility.socketClass.socketFunction(roomID, players, getWinner);
-    }
 
-    async getWinner(p, c) {
-        if (p === c) {
-            return "draw";
-        } else if (p === "rock") {
-            if (c === "paper") {
-                return "2";
+    async winGame(player, computer) {
+        const result = document.querySelector('.result');
+        const playerScoreBoard = document.querySelector('.p-count');
+        const computerScoreBoard = document.querySelector('.c-count');
+        player = player.toLowerCase();
+        computer = computer.toLowerCase();
+        if (player === computer) {
+            result.textContent = 'Tie'
+        } else if (player == 'rock') {
+            if (computer == 'paper') {
+                result.textContent = 'Computer Won';
+                computerScore++;
+                computerScoreBoard.textContent = computerScore;
+
             } else {
-                return "1";
+                result.textContent = 'Player Won'
+                playerScore++;
+                playerScoreBoard.textContent = playerScore;
             }
-        } else if (p === "paper") {
-            if (c === "scissors") {
-                return "2";
+        } else if (player == 'scissors') {
+            if (computer == 'rock') {
+                result.textContent = 'Computer Won';
+                computerScore++;
+                computerScoreBoard.textContent = computerScore;
             } else {
-                return "1";
+                result.textContent = 'Player Won';
+                playerScore++;
+                playerScoreBoard.textContent = playerScore;
             }
-        } else if (p === "scissors") {
-            if (c === "rock") {
-                return "2";
+        } else if (player == 'paper') {
+            if (computer == 'scissors') {
+                result.textContent = 'Computer Won';
+                computerScore++;
+                computerScoreBoard.textContent = computerScore;
             } else {
-                return "1";
+                result.textContent = 'Player Won';
+                playerScore++;
+                playerScoreBoard.textContent = playerScore;
             }
         }
-        return "That's not a valid choice";
     }
+
+    async playGame() {
+        const rockBtn = document.querySelector('.rock');
+        const paperBtn = document.querySelector('.paper');
+        const scissorBtn = document.querySelector('.scissor');
+        const playerOptions = [rockBtn, paperBtn, scissorBtn];
+        const computerOptions = ['rock', 'paper', 'scissors']
+
+        // Function to start playing game
+        playerOptions.forEach(option => {
+            option.addEventListener('click', function() {
+
+                const movesLeft = document.querySelector('.movesleft');
+                moves++;
+                movesLeft.innerText = `Moves Left: ${10 - moves}`;
+
+
+                const choiceNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[choiceNumber];
+
+                // Function to check who wins
+                winner(this.innerText, computerChoice)
+
+                // Calling gameOver function after 10 moves
+                if (moves == 10) {
+                    gameOver(playerOptions, movesLeft);
+                }
+            })
+        })
+    }
+
+    async overGame(playerOptions, movesLeft) {
+        const chooseMove = document.querySelector('.move');
+        const result = document.querySelector('.result');
+        const reloadBtn = document.querySelector('.reload');
+        playerOptions.forEach(option => {
+            option.style.display = 'none';
+        })
+        chooseMove.innerText = 'Game Over!!'
+        movesLeft.style.display = 'none';
+        if (playerScore > computerScore) {
+            result.style.fontSize = '2rem';
+            result.innerText = 'You Won The Game'
+            result.style.color = '#308D46';
+        } else if (playerScore < computerScore) {
+            result.style.fontSize = '2rem';
+            result.innerText = 'You Lost The Game';
+            result.style.color = 'red';
+        } else {
+            result.style.fontSize = '2rem';
+            result.innerText = 'Tie';
+            result.style.color = 'grey'
+        }
+        reloadBtn.innerText = 'Restart';
+        reloadBtn.style.display = 'flex'
+        reloadBtn.addEventListener('click', () => {
+            window.location.reload();
+        })
+
+    }
+
 }
 module.exports = new resultController;
