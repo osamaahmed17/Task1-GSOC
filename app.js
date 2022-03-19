82e3560cb0fff20239848ccf8e2aa722255ef727
osamaahmed17 <osamaahmedtahir17@gmail.com>
2022-03-19 16:35:33 +0500

    Express and Nodemon Dependencies added

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var express = expresss();
 //Server Setup
 if (process.env.PORT) {
     var server = express.listen(process.env.PORT || 80, process.env.IP, function() {
-        console.log("THe Server is running");
+        console.log("Server is running");
     });
 } else {
     var server = express.listen(3000, function() {
-        console.log("THe Server is running");
+        console.log("Server is running");
     });
 }
